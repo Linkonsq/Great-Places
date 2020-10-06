f43b1182d640de15cefe6a20571cd27fc219e4d5
Linkon Siddique <linkon.sq48@gmail.com>
2020-10-06 20:59:50 +0600

    Preparing SQLite

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/foundation.dart';
+import 'package:great_places/helpers/db_helper.dart';
 import 'package:great_places/models/place.dart';
 
 class GreatPlaces with ChangeNotifier {
@@ -19,5 +20,10 @@ class GreatPlaces with ChangeNotifier {
 
     _items.add(newPlace);
     notifyListeners();
+    DBHelper.insert('places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path
+    });
   }
 }
